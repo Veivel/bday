1a1022a11b2ce2aa17991a6f447322492d54680a
Veivel <25278964+Veivel@users.noreply.github.com>
2025-05-04 23:07:59 +0700

    fix: change forEach to for loop

--- src/wish/wish.service.ts
+++ src/wish/wish.service.ts
@@ -50,7 +50,7 @@ export class WishService {
     this.logger.debug(`Found ${users.length} users within delta...`);
 
     // check if now.utc == birthday.utc + 9 hours, send birthday
-    users.forEach((user) => {
+    for (const user of users) {
       const nextBirthWishDate: luxon.DateTime = DateTime.fromISO(
         user.nextBirthWish.toISOString(),
       ).toUTC();
@@ -66,19 +66,19 @@ export class WishService {
         const newNextBirthWishDate = nextBirthWishDate.plus(
           Duration.fromObject({ years: 1 }),
         );
-        const response = user
-          .$set({
-            nextBirthWish: newNextBirthWishDate.toISO(),
-          })
-          .save();
-        response
-          .then((res) => {
-            this.logger.debug(res);
-          })
-          .catch((err) => {
-            this.logger.error(err);
-          });
+
+        // save next birthday to user
+        try {
+          const response = await user
+            .$set({
+              nextBirthWish: newNextBirthWishDate.toISO(),
+            })
+            .save();
+          this.logger.debug(response);
+        } catch (error) {
+          this.logger.error(error);
+        }
       }
-    });
+    }
   }
 }
